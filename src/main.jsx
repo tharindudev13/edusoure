6e57b03e497d2dbc0f5cb75398ec9a4f091455d1
Tharindu Devinda <tharindukodippili13@gmail.com>
2026-05-10 07:24:27 +0530

    feat: Remove unused SpeedInsights import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store.js'
-// eslint-disable-next-line no-unused-vars
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store} >
